9f2298f2286126198c273e2bddeb08cce05ac4d9
GennadyVenidiktov <wenter3@outlook.com>
2023-08-01 16:56:32 +0500

    Comparable
     - Comparable это интерфейс для сравнения объектов

---
+++ src/main/java/ru/venidiktov/comparation/Test1.java
@@ -0,0 +1,18 @@
+package ru.venidiktov.comparation;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Test1 {
+    public static void main(String[] args) {
+        List<String> list1 = new ArrayList<String>();
+        list1.add("Ivan");
+        list1.add("Maria");
+        list1.add("Iosif");
+        System.out.println("list1 до сортировки = " + list1); //list1 без сортировки = [Ivan, Maria, Iosif]
+        //Сортировка в лексикографическом порядке (Как в словаре, слова на 'a' самые маленькие на 'z' самые большие)
+        Collections.sort(list1);
+        System.out.println("list1 после сортировки = " + list1); //list1 с сортировкой = [Iosif, Ivan, Maria]
+    }
+}
---
+++ src/main/java/ru/venidiktov/comparation/Test2.java
@@ -0,0 +1,58 @@
+package ru.venidiktov.comparation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Test2 {
+    public static void main(String[] args) {
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(22, "Ivan", "Ivanov", 100));
+        employees.add(new Employee(15, "Maria", "Marieva", 75));
+        employees.add(new Employee(15, "Maria", "Marieva", 60));
+        employees.add(new Employee(123, "Iosif", "Iosifov", 75));
+        System.out.println("list1 до сортировки = " + employees); //list1 до сортировки = [Employee{'Ivan', 'Maria', 'Iosif'}]
+        Collections.sort(employees);
+        System.out.println("list1 после сортировки = " + employees); //list1 после сортировки = [Employee{'Maria', 'Ivan', 'Iosif'}]
+//        Arrays.sort(new Employee[] {emp1, emp2, emp3}); Если класс не имплементит Comparable будет ошибка в RunTime!
+    }
+}
+
+class Employee implements Comparable<Employee> {
+    Integer id;
+    String name;
+    String surname;
+    Integer salary;
+
+    public Employee(int id, String name, String surname, int salary) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.salary = salary;
+    }
+
+    // 1 текущий объект больше o
+    // 0 текущий объект равен o
+    // -1 текущий объект меньше o
+    @Override
+    public int compareTo(Employee o) {
+//        return this.id.compareTo(o.id); //Используем для сравнения id, вызываем у Integer его метод compareTo
+//        return this.id - o.id
+        var res = this.id.compareTo(o.id);
+        if(res == 0) {
+            res = this.salary.compareTo(o.salary); //Если id равны то сравниваем salary
+        }
+        return res;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
